9490a2f3964a19c0d62a27ab950fc436312611d0
Rajat Chauhan <rajatchauhan407@gmail.com>
2022-09-24 18:32:44 +0530

    create stash.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React,{useState} from 'react';
+import React,{useEffect, useMemo, useState} from 'react';
+import { setConstantValue } from 'typescript';
 import './App.css';
 import Greeter from './components/greeter';
 import ListOfTenThings from './components/listOfThings';
@@ -13,16 +14,25 @@ export interface item{
 
 function App() {
 const [firstName, setFirstName] = useState("");
+const [num, setNum] = useState([1,2]);
 const [items, setItems] = useState<item[]>([
   {id:1, product:"bag", quantity:3},
   {id:2, product:"pencil", quantity:4}
 ]);
-
+const data = useMemo(()=>[3,5,6],[]);
 const handleName = (event:any)=>{
   setFirstName(event.target.value);
   console.log(event.target.value);
 }
+const addFunc = ()=>{
+  console.log("Hello");
+}
 
+useEffect(()=>{
+  console.log("Hey");
+  setNum(data);
+  addFunc();
+},[setNum,addFunc]);
  
 //  return React.createElement('div',{className:"App"},
 //  React.createElement(Greeter,{person:"Rajat"}),React.createElement(ShoppingList,{items:items}));
